1eaa1fb859226217ef58716408c262ee123d1b98
Anubhav Sharma <Ansharma@cigital.com>
2017-03-29 18:22:53 +0530

    'ReportCheck'

---
+++ ReadMe.txt
@@ -0,0 +1,20 @@
+ Instruction :
+ -----------------------
+ 1. Install Python 2.7
+ 2. Add the following path in the Environment Variables :1. C:\Python27\
+                                                         2. C:\Python27\Scripts\
+ 3. Open command prompt with the Admin Privilage and Install the "DOCX" Library by running the following command : "pip install python-docx"
+ 4. If installation gives error the upgrade your pip.
+ 4. Navigate to the following path : C:\Python27\Lib\site-packages\
+ 5. Replace the "docx" folder with the folder present in the zip file.
+ 6. Open the Command Prompt and run the following command : python ReportCheck.py
+ 7. Enter the full path of your ".docm" report with the report name and hit enter.
+
+ Example :
+ ---------------
+ python ReportCheck.py
+
+ Please Enter The Report Path :
+ full_path_to_the_report\report_name.docm
+
+
---
+++ ReportCheck.py
@@ -0,0 +1,210 @@
+import docx
+import os
+import re
+import win32com.client.gencache
+import sys
+
+
+#if sys.argv[1] == "-h":
+#    print '''\nReportCheck\n---------------------\nPlease give the full path of your "docm." report along with the report name.
+#    \nExample :\n------------\npython ReportCheck.py\nFull_Path_To_Report\Report_Name.docm'''
+#    sys.exit(0)
+
+try :
+    reportPath = raw_input("Please Enter The Report Path : \n")
+    reportPath = os.path.normpath(reportPath)
+    doc = docx.Document(reportPath)
+    #print len(doc.paragraphs)
+    l = []
+    for i in range(0, len(doc.paragraphs)):
+        if doc.paragraphs[i].text.strip():
+            l.append(doc.paragraphs[i].text)
+        else:
+            pass
+
+    clientName = l[0]
+    appName = l[1]
+    testType = l[2]
+
+    if l[7].startswith("For more information"):
+        asrName = l[8]
+        asrRole = l[9]
+        toName = l[10]
+        toRole = l[11]
+        mgrName = l[12]
+        mgrRole = l[13]
+    else:
+        asrName = l[7]
+        asrRole = l[8]
+        toName = l[9]
+        toRole = l[10]
+        mgrName = l[11]
+        mgrRole = l[12]
+    print "************************************************************************************************************"
+    print "\nScan Details\n=============="
+    print "Client Name : " + clientName + "\n"
+    print "Application Name : " + appName + "\n"
+    print "Test Type : " + testType + "\n"
+    if l[6].startswith("For more information"):
+        print "Client Address : None"+"\n"
+    else:
+        clientAddress = l[6]
+        print "Client Address : "+ clientAddress+"\n"
+    print "************************************************************************************************************"
+    print "Assessor Details\n=================="
+    print "Assessor's Name : " + asrName
+    print "Assessor's Designation : "+ asrRole + "\n"
+    print "TO Name : "+ toName
+    print "TO Designation : "+ toRole + "\n"
+    print "Manager Name : "+ mgrName
+    print "Manager Designation : "+ mgrRole + "\n"
+    print "************************************************************************************************************"
+    firstLine = 0
+    lastLine = 0
+    sumFindings = 0
+    exError = 0
+    intError = 0
+    totalFind = 0
+    for i in range(0,len(l)):
+        if l[i].startswith("Executive Summary"):
+            exSum = l[i+1]
+            count = sum(1 for _ in re.finditer(r'\b%s\b' % re.escape(appName), exSum))
+            if not exSum.startswith(clientName):
+                print "\nError : Client Name mismatched in executive summary. Please correct it."
+                exError += 1
+            if (not appName in l[i + 1]) or (not count == 2):
+                print "\nError : Application Name mismatched in executive summary. Please correct it."
+                exError += 1
+            if not testType in l[i + 1]:
+                print "\nError : Offering Name mismatched in executive summary. Please correct it."
+                exError += 1
+            if exError == 0:
+                print "\nExecutive Summary is correct. :)\n"
+        else:
+            pass
+        if "findings characterized as follows:" in l[i]:
+            totalFind += 1
+            firstLine =  i+1
+            p = l[i].split()
+            for k in range(0,len(p)):
+                if p[k].startswith("identified"):
+                    sumFindings = p[k+1]
+                else:
+                    pass
+        else:
+            pass
+
+        if ("While performing the assessment" in l[i]) and (totalFind != 0):
+            lastLine =  i-1
+        else:
+            pass
+        if l[i].startswith("Introduction"):
+            intro = l[i + 1]
+            count = sum(1 for _ in re.finditer(r'\b%s\b' % re.escape(appName), intro))
+            if not intro.startswith(clientName):
+                print "\nError : Client Name mismatched in "'Introduction'". Please correct it."
+                intError += 1
+            if (not appName in l[i + 1]) or (not count == 2):
+                print "\nError : Application Name mismatched in "'Introduction'". Please correct it."
+                intError += 1
+            if not testType in l[i + 1]:
+                intError += 1
+                print "\nError : Offering Name mismatched in "'Introduction'". Please correct it."
+            if intError == 0:
+                print "\nIntroduction is correct. :)\n"
+        else:
+            pass
+    print "************************************************************************************************************"
+    if totalFind != 0:
+        print "Findings Categories : \n====================="
+        a =[]
+        for line in range(firstLine,lastLine+1):
+            print l[line]
+            a.append(l[line].split())
+
+        Sum = 0
+
+        for t in range(0,len(a)):
+            Sum += int(a[t][0])
+
+        print "\nSum of findings is : ",format(Sum)
+        print "\nTotal findings written in finding categories : "+ sumFindings
+        print "************************************************************************************************************"
+        if Sum == int(sumFindings):
+            print "\nNumber of findings are matched with the Total of findings.\n"
+            print "************************************************************************************************************"
+        else:
+            print "\nThere is a mismatch between your Total Findings and Finding Categories.\nPlease correct it."
+    else :
+        print "No finding categories are found or may be you've wrong written your Executive Summary. Please Check."
+    print "************************************************************************************************************"
+
+#Checking for the Grammatical Errors and Spelling Errors
+
+    check = raw_input("\nDo you want to check the Grammar and Spelling Errors ?\nPlease press Y for yes or Enter for skip.\n")
+    check = check.upper()
+    if check == "Y":
+        print "************************************************************************************************************"
+        print "Checking the Grammar Errors.......\n"
+
+        wdDoNotSaveChanges = 0
+
+        app = win32com.client.gencache.EnsureDispatch('Word.Application')
+        doc = app.Documents.Open(reportPath)
+
+        grmError = []
+        splError = []
+
+        if doc.GrammaticalErrors.Count > 0:
+            for err in doc.GrammaticalErrors:
+                grmError.append(err.Text)
+        if doc.SpellingErrors.Count > 0:
+            for err in doc.SpellingErrors:
+                splError.append(err.Text)
+
+        if not grmError is None:
+            choice = raw_input(
+                "\nYou have %d grammar errors in the report, do you want to see it ?\nPress Y for yes or Enter for skip.\n" % (
+                doc.GrammaticalErrors.Count))
+            choice = choice.upper()
+            if choice == "Y":
+                print "************************************************************************************************************"
+                print "Printing Grammar Errors........\n"
+                for i in range(0, len(grmError)):
+                    print "Grammatical Error in : " + grmError[i].encode('utf-8')
+            else:
+                pass
+        print "************************************************************************************************************"
+        print "Checking for Spelling Errors........\n"
+        if not splError is None:
+            print "************************************************************************************************************"
+            choice = raw_input(
+                "\nYou have %d spelling mistakes in the report, do you want to see it ?\nPress Y for yes or Enter for skip.\n" % (
+                doc.SpellingErrors.Count))
+            choice = choice.upper()
+            if choice == "Y":
+                print "************************************************************************************************************"
+                print "Printing Spelling Errors........\n"
+                for i in range(0, len(splError)):
+                    print "Spelling Error in : " + splError[i].encode('utf-8')
+                print "************************************************************************************************************"
+            else:
+                pass
+
+        app.Quit(wdDoNotSaveChanges)
+    else:
+        print "************************************************************************************************************"
+        print "\nYou skipped the Grammatical and Spelling checking.\n"
+        print "************************************************************************************************************"
+except:
+    print "\nSorry ! Some Error Occur.\nMay be the report is not present at the path or you entered the wrong format.\nYou can use -h option for help.\n(python ReportCheck.py -h)"
+
+
+"""
+---------------------------
+***************************
+Anubhav Sharma
+Mail: ansharma@cigital.com
+***************************
+---------------------------
+"""
